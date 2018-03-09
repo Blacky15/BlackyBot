83b2d35682e0f9fdb569c0960eee9969c4739474
Blacky15 <gamesoftes@gmail.com>
2018-03-09 17:58:48 +0100

    Fix Test

--- src/main/java/util/Secrets.java
+++ src/main/java/util/Secrets.java
@@ -2,6 +2,6 @@ package util;
 
 public class Secrets {
 	
-	public static final String TOKEN = "SimJBr5MXiwRU3BvsJHQ932eXK_pIZn_";
+	public static final String TOKEN = "_dvlsUNV8gGnbCh1C9imm5Uc0NjsO0S4";
 
 }
